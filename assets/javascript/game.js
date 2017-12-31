339841f91a32d5f6da68b05e4e954bb99a5693b3
Rick Huynh <rickduyhuynh98@gmail.com>
2017-12-31 01:44:13 -0800

    Made all files and set baseline

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+document.write("Text written using JavaScript code!");
+
+document.onkeyup = function(event){
+	let Answer = "Leauge";
+	var guess = String.fromCharCode(event.keyCode).toLowerCase()
+	console.log(`I love ${Answer}`);
+	
+}
+
+let x = 5;
+let y = 4;
+console.log(`sum is ${x+y}`);
