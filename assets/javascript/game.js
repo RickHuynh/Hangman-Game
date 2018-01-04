9c7c3d4b73669c33efa834b956fc5a4ac26fe823
Rick Huynh <rickduyhuynh98@gmail.com>
2018-01-04 13:01:50 -0800

    finish most of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,61 @@
-document.write("Text written using JavaScript code!");
+var audio = new Audio('Music/Urf.mp3');
+audio.volume = .1;
+audio.play();
+let answerList = ["jhin","kindred"];
 
-document.onkeyup = function(event){
-	let Answer = "Leauge";
-	var guess = String.fromCharCode(event.keyCode).toLowerCase()
-	console.log(`I love ${Answer}`);
-	
+function Game(){
+	this.randomNumber = Math.floor(Math.random()*answerList.length);
+	this.answerRandom = answerList[this.randomNumber];
+	this.answerFinal = this.answerRandom.split("");
+	this.answerBlank = [this.answerRandom.length];
+	for(i=0;i<this.answerRandom.length;i++){
+		this.answerBlank[i]=("_");
+	}
+	this.answerString = this.answerBlank.join("");
 }
 
-let x = 5;
-let y = 4;
-console.log(`sum is ${x+y}`);
+var newGame = new Game();
+$( ".result_text" ).html(newGame.answerString);
+
+function isGameOver(){
+	if(newGame.answerString.indexOf("_")==-1){
+		return true;
+	}
+	return false;
+}
+
+function playMusic(){
+	audio.pause();
+	if(newGame.randomNumber==0){
+		audio = new Audio('Music/JHIN.mp3');
+	}
+	else{
+		audio = new Audio('Music/KIND.mp3');
+	}
+	audio.volume = .1;
+	audio.play();
+}
+
+document.onkeyup = function(event){
+	var guess = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log(guess);
+	if(newGame.answerRandom.indexOf(guess)!=-1){
+		console.log(`The word contains ${guess}`);
+		for(i=0;i<newGame.answerBlank.length;i++){
+			if(newGame.answerFinal[i]==guess){
+				newGame.answerBlank[i]=guess;
+			}
+		}
+		newGame.answerString=newGame.answerBlank.join("");
+		console.log(newGame.answerString);
+		$( ".result_text" ).html(newGame.answerString);
+		if(isGameOver()){
+			playMusic();
+			answerList.splice(newGame.randomNumber,newGame.randomNumber+1);
+			newGame = new Game();
+		}
+	}	
+	else{
+		console.log(`The word does not contain ${guess}`);
+	}
+}
